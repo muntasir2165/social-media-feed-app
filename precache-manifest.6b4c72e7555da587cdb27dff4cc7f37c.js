737899a8598ffb9f7dbf9963653ff09ebe3dde74
muntasir2165 <muntasir2165@hotmail.com>
2019-03-11 15:44:49 -0700

    Updates

---
+++ precache-manifest.6b4c72e7555da587cdb27dff4cc7f37c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11ee6ebddd271bc5f6b7",
+    "url": "/social-media-feed-app/static/js/runtime~main.cf14572d.js"
+  },
+  {
+    "revision": "084545dd3b3c8a3db016",
+    "url": "/social-media-feed-app/static/js/main.1ab165c0.chunk.js"
+  },
+  {
+    "revision": "37633691b9724b784dbc",
+    "url": "/social-media-feed-app/static/js/2.ff439d2d.chunk.js"
+  },
+  {
+    "revision": "084545dd3b3c8a3db016",
+    "url": "/social-media-feed-app/static/css/main.f0d780a1.chunk.css"
+  },
+  {
+    "revision": "9e07c077650c64a283b5191705d932f7",
+    "url": "/social-media-feed-app/index.html"
+  }
+];
